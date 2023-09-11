70b388094443c16121fdd5c3c3b131812b23e153
basma <basma@BASMALA_HOME>
2023-09-11 11:54:51 +0300

    Add project files.

---
+++ Employees/Data/Entities/Departmenets.cs
@@ -0,0 +1,10 @@
+﻿namespace Employees.Data.Entities
+{
+    public class Departmenets
+    {
+        public Guid DepId { get; set; }
+        public string DepName { get; set; }
+        public string? Building { get; set; }
+
+    }
+}
