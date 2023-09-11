2084192468ee0d1f78075197c045844aa9dbc4cc
BasmalaHesho <basmala.hesho@gmail.com>
2023-09-11 21:21:21 +0300

    Project

---
+++ Employees/Data/Entities/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Employees.Data.Entities
+{
+    public class Employee
+    {
+
+        public Guid Id { get; set; }
+        public string? Name { get; set; }
+        public required string Position { get; set; }
+        public required string Mail { get; set; }
+        public double Salary { get; set; }
+        public int Mobile { get; set; }
+        public string? Gender { get; set; }
+
+        public Guid DepId { get; set; }
+
+        [ForeignKey(nameof(DepId))]
+        public required Departmenets Departments { get; set; }
+
+    }
+}
